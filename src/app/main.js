c9d46d1dcf52bcf909821a1662945c974f53cfc5
Adam Oswalt <adam.oswalt@gmail.com>
2016-06-06 10:43:36 -0500

    dynamic addreesses populating

--- src/app/main.js
+++ src/app/main.js
@@ -6,11 +6,39 @@ angular.module("app", ["ngRoute", "ui.bootstrap"])
         controllerAs: "main",
         templateUrl: "app/partials/main.html"
       })
+      .when("/addresses", {
+        controller: "AddressCtrl",
+        controllerAs: "address",
+        templateUrl: "app/partials/addresses.html"
+      })
       .otherwise("/")
   ))
   .controller("MainCtrl", function () {
-    const main = this
-
-    main.closeAlert = (index) => main.alerts.splice(index, 1)
-
+    const main = this;
   })
+  .controller("AddressCtrl", function () {
+    const address = this;
+
+    address.list = {};
+    address.list.a1 = {
+      name: "Amy",
+      phone: "(615) 234-5678",
+      email: "amy@example.com",
+      twitter: "@amy"
+    };
+    address.list.a2 = {
+      name: "Bob",
+      phone: "(615) 345-6789",
+      email: "bob@example.com",
+      twitter: "@bob"
+    };
+  })
+
+  .filter("phonedial", () =>
+    //NOTE(adam): assuming 10 digit number
+    input => `+1${input.replace(/[^\d]/g, "")}`
+  )
+
+  .filter("twitter", () =>
+    input => input.slice(1)
+  );
